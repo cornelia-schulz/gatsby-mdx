b274fd1ae33b8993e92c09213331ea9dc0906a84
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-08-17 19:07:59 +1000

    set up bare bones gatsby blog

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => (
+    <>
+<h1>Hello Friends!</h1>
+<p>This is my homepage</p>
+<Link to="/about">Learn about me &rarr;</Link>
+</>
+)
