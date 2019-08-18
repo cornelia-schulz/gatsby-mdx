6538c4892e982920fe2c9d07f7e22049e46e40ae
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-08-18 13:05:27 +1000

    add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layoutLayout'
 
 export default () => (
-    <>
+    <Layout>
 <h1>Hello Friends!</h1>
 <p>This is my homepage</p>
 <Link to="/about">Learn about me &rarr;</Link>
-</>
+</Layout>
 )
